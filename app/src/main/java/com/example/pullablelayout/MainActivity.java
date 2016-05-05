bb658a00923e492122b06c0a4774461f5aa1f962
宗仁 <imzongren@126.com>
2016-05-05 15:18:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pullablelayout/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.pullablelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tk.zongren.pullablelayout.PullabelListView;
+import tk.zongren.pullablelayout.PullableLayout;
+import tk.zongren.pullablelayout.PullableTextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int mRefreshedTimes = 0;
+    private int mLoadedTimes = 0;
+    private PullableLayout mPullableLayout;
+    private PullableTextView mPullableTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPullableLayout = (PullableLayout)findViewById(R.id.activity_main_pullableLayout);
+        mPullableTextView = (PullableTextView) findViewById(R.id.activity_main_pullableTextView);
+        mPullableLayout.setOnPullDownListener(new PullableLayout.OnPullDownListener() {
+            @Override
+            public void onPullDown(float scrollY) {
+
+            }
+
+            @Override
+            public void onPullDownCanceled() {
+
+            }
+
+            @Override
+            public void onPullDownFinished() {
+
+            }
+        });
+        mPullableLayout.setOnPullUpListener(new PullableLayout.OnPullUpListener() {
+            @Override
+            public void onPullUp() {
+
+            }
+
+            @Override
+            public void onPullUpCanceled() {
+
+            }
+
+            @Override
+            public void onPullUpFinished() {
+
+            }
+        });
+        mPullableLayout.setOnRefreshListener(new PullableLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh(PullableLayout pullableLayout) {
+                new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            mPullableLayout.refreshFinish(PullableLayout.SUCCEED);
+                            mRefreshedTimes ++;
+                            updateTextView();
+                        }
+                    },
+                1000);
+            }
+
+            @Override
+            public void onLoadMore(PullableLayout pullableLayout) {
+                new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            mPullableLayout.loadMoreFinish(PullableLayout.SUCCEED);
+                            mLoadedTimes ++;
+                            updateTextView();
+                        }
+                    },
+                1000);
+            }
+        });
+        updateTextView();
+    }
+
+    private void updateTextView() {
+        mPullableTextView.setText(String.format(getResources().getString(R.string.refresh_and_load_times),mRefreshedTimes,mLoadedTimes));
+    }
+
+
+}
